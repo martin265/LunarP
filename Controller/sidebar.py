163b8fb789720dd9e9ce044198cac4e87d4e2922
martin265 <martinsilungwe12@gmail.com>
2024-02-15 20:31:56 +0200

    second commit

--- Controller/sidebar.py
+++ Controller/sidebar.py
@@ -5,5 +5,11 @@ class SidebarMenu:
     def __init__(self, page: ft.Page):
         super().__init__()
         self.page = page
+    
+        # =========== the main side navigation for the desktop application will be here ======== //
+        self.navigation_rail = ft.NavigationRail(
+            
+        )
 
     def build(self):
+        
